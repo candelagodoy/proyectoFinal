c7df055f42b9050b7f399c80f169d42d0554551f
Segu33 <luis.segura3@hotmail.com>
2024-06-01 13:08:02 -0300

    GimnasioG18 entidades

---
+++ src/gimnasiog18/entidades/Asistencia.java
@@ -0,0 +1,20 @@
+
+package gimnasiog18.entidades;
+
+import java.util.Date;
+
+
+public class Asistencia {
+    private int IdAsistencia;
+    private int IdSocio;
+    private int IdClase;
+    private Date FechaAsistencia;
+}
+public Asistencia (){
+
+}
+public Asistencia (int IdAsistencia, int IdSocio, int IdClase, Date FechaAsistencia){
+
+
+
+}
